34c29e6fb607c7c38b902e8799b12592fd15f5bb
Boguste Hameyie <bhameyie@idasile.com>
2017-06-12 02:08:06 -0400

    added tests for the importer app

--- importer/src/main/scala/com/bhameyie/suggester/importer/Main.scala
+++ importer/src/main/scala/com/bhameyie/suggester/importer/Main.scala
@@ -1,6 +1,7 @@
 package com.bhameyie.suggester.importer
 
 object Main {
+
   import scala.concurrent.Await
   import scala.language.postfixOps
 
@@ -19,7 +20,10 @@ object Main {
   private val decider: Supervision.Decider = _ => Supervision.Stop
 
   def main(args: Array[String]): Unit = {
+    ParameterParser().parse(args, RunParameters()).foreach(run)
+  }
 
+  def run(param: RunParameters): Unit = {
     implicit val system = {
       val s = ActorSystem("Suggester")
       sys.addShutdownHook(s.terminate())
@@ -31,40 +35,33 @@ object Main {
 
     implicit val executionContext = system.dispatcher
 
-    ParameterParser().parse(args, RunParameters()) match {
+    val adminCodeCache = AdminCodeCache(param.adminCodeFile.toPath)
 
-      case None =>
+    val recordsCollection: MongoCollection[DatabaseCityRecord] = {
+      val mongoDatabase = ApplicationDatabase(param.mongoUrl, param.mongoDb)
+
+      val coll = mongoDatabase.getCollection[DatabaseCityRecord](Collections.cityRecords)
+      coll.createIndex(Document("location" -> "2dsphere")).toFuture
+      coll
+    }
+
+    val pumper = DataPumper.pump(recordsCollection) _
+    val processCity = LineProcessor.process(adminCodeCache) _
+
+    FileIO.fromPath(param.dataFile.toPath)
+      .via(Framing.delimiter(ByteString("\n"), Int.MaxValue))
+      .map(_.utf8String)
+      .map(processCity)
+      .mapAsync(4)(pumper)
+      .to(Sink.ignore).run().onComplete {
+
+      case Failure(exception) =>
+        logger.error(s"An error occurred while processing the stream. ${exception.getMessage}", exception)
+      case Success(_) =>
+
+        logger.debug("COMPLETED SUCCESSFULLY")
         Await.ready(system.terminate(), 10 seconds)
 
-      case Some(param) =>
-        val adminCodeCache = AdminCodeCache(param.adminCodeFile.toPath)
-
-        val recordsCollection: MongoCollection[DatabaseCityRecord] = {
-          val mongoDatabase = ApplicationDatabase(param.mongoUrl, param.mongoDb)
-
-          val coll = mongoDatabase.getCollection[DatabaseCityRecord](Collections.cityRecords)
-          coll.createIndex(Document("location" -> "2dsphere")).toFuture
-          coll
-        }
-
-        val pumper = DataPumper.pump(recordsCollection) _
-        val processCity = LineProcessor.process(adminCodeCache) _
-
-        FileIO.fromPath(param.dataFile.toPath)
-          .via(Framing.delimiter(ByteString("\n"), Int.MaxValue))
-          .map(_.utf8String)
-          .map(processCity)
-          .mapAsync(4)(pumper)
-          .to(Sink.ignore).run().onComplete {
-
-          case Failure(exception) =>
-            logger.error(s"An error occurred while processing the stream. ${exception.getMessage}", exception)
-          case Success(_) =>
-
-            logger.debug("COMPLETED SUCCESSFULLY")
-            Await.ready(system.terminate(), 10 seconds)
-
-        }
     }
   }
 }
--- importer/src/test/scala/com/bhameyie/suggester/importer/LineProcessorSpecs.scala
+++ importer/src/test/scala/com/bhameyie/suggester/importer/LineProcessorSpecs.scala
@@ -1,7 +1,14 @@
 package com.bhameyie.suggester.importer
 
-import org.scalatest.{Matchers, WordSpec}
+import java.io.File
+
+import com.bhameyie.suggester.database.{ApplicationDatabase, Collections, DatabaseCityRecord}
+import com.typesafe.config.ConfigFactory
+import org.mongodb.scala.Document
 import org.scalatest.prop.Checkers
+import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpec}
+
+import scala.concurrent.Await
 
 class LineProcessorSpecs extends WordSpec with Matchers with Checkers {
 
@@ -38,5 +45,66 @@ class LineProcessorSpecs extends WordSpec with Matchers with Checkers {
       unified.head shouldEqual unified(2)
       unified.head shouldEqual unified(3)
     }
+  }
+}
+
+class AppIntegrationSpecs extends WordSpec with Matchers with Checkers with BeforeAndAfterAll {
+  import scala.concurrent.duration._
+  private val currentDir = System.getProperty("user.dir")
+
+  private val conf = ConfigFactory.load()
+  private val db = ApplicationDatabase(conf)
+
+  private val citiesColl = {
+    val coll = db.getCollection[DatabaseCityRecord](Collections.cityRecords)
+    coll.createIndex(Document("location" -> "2dsphere")).toFuture
+    coll
+  }
+
+  override def beforeAll(): Unit = {
+    Await.result(citiesColl.drop().toFuture, 10 seconds)
+  }
+
+  "run" should{
+
+    "load tsv" in{
+
+      val param = RunParameters(new File("data/small.tsv"),new File("data/small_codes.tsv"),
+        "mongodb://localhost","suggester")
+      Main.run(param)
+
+      Thread.sleep(3000)
+
+      val res= Await.result( citiesColl.find().toFuture(), 3 seconds)
+
+      res.size shouldBe 7
+
+      val row1=res.filter(_.name.startsWith("Abb"))
+      row1.size shouldBe 4
+      row1.map(_.name) shouldEqual List("Abbotsfordy", "Abb", "Abbotsford", "Abbotsfordo")
+    }
+
+    "can load tsv multiple times to same number" in{
+
+      val param = RunParameters(new File("data/small.tsv"),new File("data/small_codes.tsv"),
+        "mongodb://localhost","suggester")
+      Main.run(param)
+
+      Thread.sleep(2000)
+
+      val res= Await.result( citiesColl.find().toFuture(), 3 seconds)
+
+      res.size shouldBe 7
+
+      Main.run(param)
+
+      Thread.sleep(2000)
+
+
+      val res2= Await.result( citiesColl.find().toFuture(), 3 seconds)
+
+      res2.size shouldBe res.size
+    }
+
   }
 }
